5baa28dd5ec3be5760d5127b191711d36f5b12b4
Dulina <dulinagunasekara@gmail.com>
2023-11-11 17:33:39 -0500

    added temp

---
+++ frontend/src/app/(site)/page.js
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import Chat from '../components/Chat'
+
+export default function Home() {
+  return (
+  <>
+  
+  
+  <h1>ray report</h1>
+
+  <Chat/>
+  
+  </>
+  )
+}
