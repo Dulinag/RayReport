97e90922a3c780dfda27e37fc19a5211252ef0c4
mahathirrojan <torbengalifreind@gmail.com>
2023-11-12 01:51:09 -0500

    Styled the front end to give the website with a sky theme

--- frontend/src/app/components/CenterText.jsx
+++ frontend/src/app/components/CenterText.jsx
@@ -4,11 +4,17 @@ function CenterText() {
     return (
         <div className="flex items-center justify-center h-full">
         <div className="text-gray-600 text-center">
-          <div className="bg-blue-500 text-white rounded-md p-4">
+        <div className="bg-gray-100 rounded-full p-8 relative overflow-hidden">
+          <div className="absolute w-16 h-8 bg-gray-100 rounded-full top-0 left-0 transform -rotate-45"></div>
+          <div className="absolute w-16 h-8 bg-gray-100 rounded-full top-0 right-0 transform rotate-45"></div>
+          
+          
+          <div className="p-4">
             <p className="text-lg font-semibold">Welcome to RayReport!</p>
-            <p className="text-sm">Your Friendly UV bot!</p>
+            <p className="text-sm">Your Friendly UV reporter!</p>
           </div>
         </div>
+      </div>
       </div>
     );
   }
