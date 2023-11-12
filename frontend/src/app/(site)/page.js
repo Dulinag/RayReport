07bb334277d93ea3b648cb88c2adaf39ac617dad
mahathirrojan <torbengalifreind@gmail.com>
2023-11-11 19:27:40 -0500

    added Center Text in the website

--- frontend/src/app/(site)/page.js
+++ frontend/src/app/(site)/page.js
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import Chat from '../components/Chat'
+import CenterText from '../components/CenterText'
 
 export default function Home() {
   return (
   <>
   
   
-
+  <CenterText/>
   <Chat/>
   
   </>
---
+++ frontend/src/app/components/CenterText.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function CenterText() {
+    return (
+        <div className="flex items-center justify-center h-full">
+        <div className="text-gray-600 text-center">
+          <div className="bg-blue-500 text-white rounded-md p-4">
+            <p className="text-lg font-semibold">Welcome to RayReport!</p>
+            <p className="text-sm">Your Friendly UV bot!</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default CenterText;
+
